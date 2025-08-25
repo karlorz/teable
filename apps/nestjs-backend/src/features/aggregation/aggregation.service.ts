d6b88b1dbcf7bfce4cd0152999215eeac7ca9a9d
nichenqin <nichenqin@hotmail.com>
2025-08-25 15:58:45 +0800

    fix: fix search

--- apps/nestjs-backend/src/features/aggregation/aggregation.service.ts
+++ apps/nestjs-backend/src/features/aggregation/aggregation.service.ts
@@ -757,6 +757,7 @@ export class AggregationService implements IAggregationService {
       searchFields,
       queryRo,
       tableIndex,
+      undefined, // context
       basicSortIndex,
       filterQuery,
       sortQuery
