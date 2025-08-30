a3f0e52884986f33607d6bdcd8ea3cc26c1ee256
nichenqin <nichenqin@hotmail.com>
2025-08-30 19:18:13 +0800

    fix: fix query database view cache

--- apps/nestjs-backend/src/app.module.ts
+++ apps/nestjs-backend/src/app.module.ts
@@ -85,7 +85,7 @@ export const appModules = {
     AiModule,
     PluginPanelModule,
     PluginContextMenuModule,
-    DatabaseViewModule,
+    // DatabaseViewModule,
   ],
   providers: [InitBootstrapProvider],
 };
